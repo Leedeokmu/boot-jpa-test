db22d915cb0a431004b666d5060453bef9904dc7
freeefly <freefly3557@gmail.com>
2020-03-17 22:41:00 +0900

    feat : set environment

--- src/test/java/jpabook/jpashop/JpashopApplicationTest.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTest.java
@@ -1,10 +1,10 @@
 package jpabook.jpashop;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JpashopApplicationTests {
+class JpashopApplicationTest {
 
     @Test
     void contextLoads() {
